4099012f8f713eb61441e8f6a88f32c0c874b831
Wenson Zhou <wenson.zhou@hotmail.com>
2013-06-03 18:46:30 +0800

    Add StepLogger support to default logging module

--- bootstrap/src/condotti/logging.js
+++ bootstrap/src/condotti/logging.js
@@ -301,13 +301,86 @@ Condotti.add('condotti.logging', function (C, config) {
      * @return {Condotti} the calling Condotti instance
      */
     ['fatal', 'error', 'warn', 'info', 'debug', 'trace'].forEach( 
-                   function (name, index) {
-                       C[name] = function (message) {
-                           if (C.logger_ && C.logger_[name]) {
-                               C.logger_[name](message);
-                           }
-                       };
-                       return true;
-                   });
+        function (name, index) {
+           C[name] = function (message) {
+               if (C.logger_ && C.logger_[name]) {
+                   C.logger_[name](message);
+               }
+           };
+           return true;
+        }
+    );
+    
+    /**
+     * This StepLogger class is designed as a helper, and provides the facility
+     * to log the begin, end of a step with the specified message. If the step
+     * ends with an error, the logger is to log the error details. On the other
+     * hand, if the step succeeds, it will log down the result too.
+     * 
+     * @class StepLogger
+     * @constructor
+     * @param {Logger} logger the logger instance used to complete the logging
+     */
+    function StepLogger (logger) {
+        /**
+         * The message used to construct the log
+         * 
+         * @property message_
+         * @type String
+         * @default ''
+         */
+        this.message_ = '';
+        
+        /**
+         * The logger instance to complete the logging functionality
+         * 
+         * @property logger_
+         * @type Logger
+         */
+        this.logger_ = logger;
+    }
+    
+    /**
+     * Log the begin of a step with the specified message
+     * 
+     * @method start
+     * @param {String} message the descriptive message about the step
+     */
+    StepLogger.prototype.start = function (message) {
+        this.message_ = message;
+        this.logger_.debug(message + ' ...');
+    };
+    
+    /**
+     * Log the end of a successful step with the result.
+     * 
+     * @method done
+     * @param {Object} result the result of the successful step. It can be null
+     *                        or undefined.
+     */
+    StepLogger.prototype.done = function (result) {
+        var message = this.message_ + ' succeed.';
+        if (result) {
+            message += ' Result: ' + C.lang.reflect.inspect(result);
+        }
+        
+        this.logger_.debug(message);
+    };
+    
+    /**
+     * Log the end of a failed step with the passed-in error
+     * 
+     * @method error
+     * @param {Error} error the error causes this step fail
+     */
+    StepLogger.prototype.error = function (error) {
+        this.logger_.error(this.message_ + ' failed. Error: ' + 
+                           C.lang.reflect.inspect(error));
+    };
+    
+    L.StepLogger = StepLogger;
+    L.getStepLogger = function (logger) {
+        return new StepLogger(logger);
+    };
     
 }, '0.0.1', { requires: ['condotti.lang', 'condotti.errors'] });
